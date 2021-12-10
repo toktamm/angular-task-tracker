5bc0a2ebab53c179a4d04e4385c7fe3118f914fa
Toktam <ttmohebbi@gmail.com>
2021-12-10 16:53:08 -0500

    declare task that is being passed in as an Input and its interface

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../Task'
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-
+  @Input() task: Task
   constructor() { }
 
   ngOnInit(): void {
