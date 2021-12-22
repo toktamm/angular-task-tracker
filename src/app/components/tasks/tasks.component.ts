13002d92bcf5dcffea1c7841f0e59c9733b1d98d
Toktam <ttmohebbi@gmail.com>
2021-12-22 01:15:15 -0500

    remove TASKS from the tasks component and set it to an empty array

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Task} from '../../Task'
-import {TASKS} from '../../mock-tasks'
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
   constructor() { }
 
