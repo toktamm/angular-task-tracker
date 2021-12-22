7e3d930a2abe687af5fd1e441169d5e68b528e01
Toktam <ttmohebbi@gmail.com>
2021-12-22 01:19:35 -0500

    import TaskService and add it as a provider to the constructor

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import {Task} from '../../Task'
 @Component({
   selector: 'app-tasks',
